09ccc2719e585435436cdd15b22ade035c181527
Emil_Andreasyan <emil.andreasyan@yahoo.com>
2020-08-24 12:25:25 -0700

    adding validations

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,4 +1,5 @@
 class Country < ApplicationRecord
     belongs_to :user
     has_many :cities
+    validates_presence_of :name, :capital, uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     has_secure_password
     has_many :countries
     has_many :cities, through: :countries
+    validates_presence_of :email, uniqueness: true
+    validates_presence_of :password, length: {in: 6..20}
 end
